321d93117779959458d9bf8824e88ec29e52b527
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-18 16:31:38 +0200

    ajout du background menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace Template_WPF
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Template_WPF
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GrdHeader_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
     }
 }
