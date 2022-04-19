42f604329252d6e43293e5f1a971e3c38e5c55e3
LaM0uette <lam0uette.dev@gmail.com>
2022-04-19 11:25:00 +0200

    init - refonte bases avec metrowindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,10 @@
-﻿using System.Windows;
-using System.Windows.Input;
-
-namespace Template_WPF
+﻿namespace Template_WPF
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void GrdHeader_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
         }
     }
 }
